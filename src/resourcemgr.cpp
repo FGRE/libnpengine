b72dc1a0ae0cdb54c9497145420743cd08feaf11
Mislav Blažević <krofnica996@gmail.com>
2013-10-25 10:46:02 +0200

    Work on resource manager

--- src/resourcemgr.cpp
+++ src/resourcemgr.cpp
@@ -1,23 +1,46 @@
 #include "resourcemgr.hpp"
 #include "npafile.hpp"
+#include "nsbfile.hpp"
+
+#include <memory>
+#include <algorithm>
 
 ResourceMgr::ResourceMgr(const std::vector<std::string>& AchieveFileNames)
 {
-    FileRegistry.resize(AchieveFileNames.size());
+    Achieves.resize(AchieveFileNames.size());
     for (uint32_t i = 0; i < AchieveFileNames.size(); ++i)
     {
         NpaFile* pAchieve = new NpaFile(AchieveFileNames[i], NPA_READ);
-        FileRegistry[i].first = pAchieve;
+        Achieves[i] = pAchieve;
         for (NpaIterator File = pAchieve->Begin(); File != pAchieve->End(); ++File)
-            FileRegistry[i].second[File.GetFileName()] = File.GetOffset();
+            FileRegistry.insert(std::pair<std::string, NpaIterator>(File.GetFileName(), File));
     }
+}
+
+ResourceMgr::~ResourceMgr()
+{
+    std::for_each(Achieves.begin(), Achieves.end(), std::default_delete<NpaFile>());
+    ClearCache();
 }
 
 void ResourceMgr::ClearCache()
 {
+    // TODO
 }
 
-NsbFile* ResourceMgr::GetScript(const std::string& Path)
+template <class T> T* ResourceMgr::GetResource(const std::string& Path)
 {
+    // Check cache
+    if (T* pCache = CacheHolder<T>::Read(Path))
+        return pCache;
+
+    // Check achieves
+    if (char* Data = FileRegistry[Path].GetFileData())
+    {
+        T* pScript = new T(Path, Data);
+        CacheHolder<T>::Write(Path, pScript);
+        return pScript;
+    }
+
     return nullptr;
 }
