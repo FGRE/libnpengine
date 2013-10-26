a7b1041a0d57b802b03d96ba655e7ea111f04160
Mislav Blažević <krofnica996@gmail.com>
2013-10-26 09:38:41 +0200

    Fix ResourceManager Read/Write

--- include/resourcemgr.hpp
+++ include/resourcemgr.hpp
@@ -14,8 +14,19 @@ class NpaFile;
 template <class T>
 struct CacheHolder
 {
-    static T* Read(const std::string& Path) { return Cache[Path]; }
-    static void Write(const std::string& Path, T* Data) { Cache[Path] = Data; }
+    static T* Read(const std::string& Path)
+    {
+        auto iter = Cache.find(Path);
+        if (iter != Cache.end())
+            return iter->second;
+        return nullptr;
+    }
+
+    static void Write(const std::string& Path, T* Data)
+    {
+        Cache[Path] = Data;
+    }
+
     static std::map<std::string, T*> Cache;
 };
 
@@ -31,11 +42,30 @@ public:
     ~ResourceMgr();
 
     void ClearCache();
+
+    char* Read(const std::string& Path);
     template <class T> T* GetResource(const std::string& Path);
 
 private:
     std::map<std::string, NpaIterator> FileRegistry;
     std::vector<NpaFile*> Achieves;
 };
+
+template <class T> T* ResourceMgr::GetResource(const std::string& Path)
+{
+    // Check cache
+    if (T* pCache = CacheHolder<T>::Read(Path))
+        return pCache;
+
+    // Check achieves
+    if (char* Data = Read(Path))
+    {
+        T* pScript = new T(Path, Data);
+        CacheHolder<T>::Write(Path, pScript);
+        return pScript;
+    }
+
+    return nullptr;
+}
 
 #endif
