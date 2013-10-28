a637a2477df4369b9386d717710606e5dc40b42c
Mislav Blažević <krofnica996@gmail.com>
2013-10-28 18:20:38 +0100

    Start working on function calls (to different scripts)

--- include/nsbinterpreter.hpp
+++ include/nsbinterpreter.hpp
@@ -15,6 +15,12 @@ struct Variable
     std::string Value;
 };
 
+struct FuncReturn
+{
+    NsbFile* pScript;
+    uint32_t SourceLine;
+};
+
 class NsbInterpreter
 {
 public:
@@ -23,6 +29,7 @@ public:
 
     void Run();
     void CallScript(const std::string& FileName);
+    void LoadScript(const std::string& FileName);
 
 private:
     bool Boolify(const std::string& String);
@@ -30,6 +37,7 @@ private:
     template <class T> T* GetHandle(const std::string& Identifier);
     template <class T> T GetVariable(const std::string& Identifier);
     void SetVariable(const std::string& Identifier, const Variable& Var);
+    bool CallFunction(NsbFile* pScript, const char* FuncName);
 
     void LoadMovie(const std::string& HandleName, int32_t Priority, int32_t x,
                    int32_t y, bool Loop, bool unk0, const std::string& File, bool unk1);
@@ -37,8 +45,10 @@ private:
     Game* pGame;
     ResourceMgr* pResourceMgr;
     bool EndHack;
-    std::stack<uint32_t> ReturnLines;
-    std::stack<NsbFile*> ScriptStack;
+
+    std::stack<FuncReturn> Returns;
+    std::stack<NsbFile*> ScriptStack; // TODO: Obsolete
+    std::vector<NsbFile*> LoadedScripts;
     std::map<std::string, Variable> Variables;
     std::map<std::string, void*> Handles;
     std::vector<Variable> Params;
