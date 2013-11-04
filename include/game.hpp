fb7d1e6d4d52bdee2cd35198a08077c6d8cdeff3
Mislav Blažević <krofnica996@gmail.com>
2013-11-04 09:45:27 +0100

    Partial support for textures, use sfe::Movie directly, add copyright notices

--- include/game.hpp
+++ include/game.hpp
@@ -1,10 +1,27 @@
+/* 
+ * libnpengine: Nitroplus script interpreter
+ * Copyright (C) 2013 Mislav Blažević <krofnica996@gmail.com>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * */
 #ifndef GAME_HPP
 #define GAME_HPP
 
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Window/Keyboard.hpp>
 #include <list>
-#include "movie.hpp"
+#include "drawable.hpp"
 #include "nsbinterpreter.hpp"
 
 struct Callback
@@ -23,12 +40,12 @@ public:
     void Run();
 
 private:
-    void AddDrawable(Movie* pMovie);
-    void RemoveDrawable(Movie* pMovie);
+    void AddDrawable(Drawable Obj);
+    void RemoveDrawable(sf::Drawable* pDrawable);
     void RegisterCallback(sf::Keyboard::Key Key, const std::string& Script);
 
     std::vector<Callback> Callbacks;
-    std::list<Movie*> Movies;
+    std::list<Drawable> Drawables;
     bool IsRunning;
     NsbInterpreter* pInterpreter;
 };
