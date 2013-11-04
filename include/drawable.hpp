b8ec32ccbe2a063d72d3b9c171092296e2f34dba
Mislav Blažević <krofnica996@gmail.com>
2013-11-04 10:30:21 +0100

    Add forgotten files (drawable) - Version 0.0.1

---
+++ include/drawable.hpp
@@ -0,0 +1,52 @@
+/* 
+ * libnpengine: Nitroplus script interpreter
+ * Copyright (C) 2013 Mislav Blažević <krofnica996@gmail.com>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * */
+#ifndef DRAWABLE_HPP
+#define DRAWABLE_HPP
+
+#include <cstdint>
+
+enum
+{
+    DRAWABLE_MOVIE      = 0,
+    DRAWABLE_TEXTURE    = 1
+};
+
+namespace sf
+{
+    class Drawable;
+};
+
+class Drawable
+{
+public:
+    Drawable(sf::Drawable* pDrawable, int32_t Priority, bool Blocking, uint8_t Type);
+
+    bool operator==(const Drawable& Other) const;
+    bool IsBlocking() const;
+    int32_t GetPriority() const;
+    sf::Drawable* Get() const;
+    bool ShouldRemove() const;
+
+private:
+    sf::Drawable* pDrawable;
+    int32_t Priority;
+    bool Blocking;
+    uint8_t Type;
+};
+
+#endif
---
+++ src/drawable.cpp
@@ -0,0 +1,55 @@
+/* 
+ * libnpengine: Nitroplus script interpreter
+ * Copyright (C) 2013 Mislav Blažević <krofnica996@gmail.com>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * */
+#include "drawable.hpp"
+
+#include <sfeMovie/Movie.hpp>
+
+Drawable::Drawable(sf::Drawable* pDrawable, int32_t Priority, bool Blocking, uint8_t Type) :
+pDrawable(pDrawable),
+Priority(Priority),
+Blocking(Blocking),
+Type(Type)
+{
+}
+
+bool Drawable::operator==(const Drawable& Other) const
+{
+    return this->pDrawable == Other.pDrawable;
+}
+
+bool Drawable::IsBlocking() const
+{
+    return Blocking;
+}
+
+int32_t Drawable::GetPriority() const
+{
+    return Priority;
+}
+
+sf::Drawable* Drawable::Get() const
+{
+    return pDrawable;
+}
+
+bool Drawable::ShouldRemove() const
+{
+    if (Type == DRAWABLE_MOVIE)
+        return static_cast<sfe::Movie*>(pDrawable)->getStatus() != sfe::Movie::Playing;
+    return false;
+}
