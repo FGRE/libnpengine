FGRE/libnpengine

commit 07d3e7681bc07d5344246503c22a8c057861477b

Disallow creating/binding identifiers to same Tree twice.

src/nsbbuiltins.cpp
@@ -397,6 +397,11 @@ void NsbInterpreter::NSBSystem(string Command, string Parameters, string Directo
 
 void NsbInterpreter::NSBCreateArray()
 {
+    // Check if tree already exists
+    auto iter = Arrays.find(pLine->Params[0]);
+    if (NsbAssert(iter == Arrays.end(), "Cannot create tree % as it already exists", pLine->Params[0]))
+        return;
+
     // Create new tree
     if (ArrayParams.empty())
         for (uint32_t i = 1; i < Params.size(); ++i)
@@ -410,6 +415,11 @@ void NsbInterpreter::NSBCreateArray()
 
 void NsbInterpreter::NSBBindIdentifier()
 {
+    // Check if identifiers are already bound
+    if (NsbAssert(Arrays[pLine->Params[0]].Members[0].first.empty(),
+        "Cannot bind identifiers to tree as they are already bound"))
+        return;
+
     // Bind to first level of tree
     if (ArrayParams.empty())
         for (uint32_t i = 1; i < Params.size(); ++i)

include/drawable.hpp
@@ -105,6 +105,4 @@ private:
     sf::Vector2f Position;
 };
 
-extern const float FadeConvert;
-
 #endif

src/nsbinterpreter.cpp
@@ -138,6 +138,7 @@ void NsbInterpreter::ExecuteScript(const string& ScriptName)
 
 void NsbInterpreter::ExecuteScriptLocal(const string& ScriptName)
 {
+    // This leaks memory but nobody really cares
     pScript = new NsbFile(ScriptName);
     Run();
 }

commit 366c0bf049fc128f33de4e7b99bbf40b5b8da57f

Fix creating subtrees. Broken in 07d3e7681bc07d5344246503c22a8c057861477b

src/nsbbuiltins.cpp
@@ -397,15 +397,18 @@ void NsbInterpreter::NSBSystem(string Command, string Parameters, string Directo
 
 void NsbInterpreter::NSBCreateArray()
 {
-    // Check if tree already exists
-    auto iter = Arrays.find(pLine->Params[0]);
-    if (NsbAssert(iter == Arrays.end(), "Cannot create tree % as it already exists", pLine->Params[0]))
-        return;
-
     // Create new tree
     if (ArrayParams.empty())
+    {
+        // Check if tree already exists
+        auto iter = Arrays.find(pLine->Params[0]);
+        if (NsbAssert(iter == Arrays.end(),
+            "Cannot create tree % as it already exists", pLine->Params[0]))
+            return;
+
         for (uint32_t i = 1; i < Params.size(); ++i)
             Arrays[pLine->Params[0]].Members.push_back(std::make_pair(string(), ArrayVariable(Params[i])));
+    }
     // Create subtree
     else
         for (uint32_t i = 1; i < Params.size(); ++i)
@@ -415,15 +418,17 @@ void NsbInterpreter::NSBCreateArray()
 
 void NsbInterpreter::NSBBindIdentifier()
 {
-    // Check if identifiers are already bound
-    if (NsbAssert(Arrays[pLine->Params[0]].Members[0].first.empty(),
-        "Cannot bind identifiers to tree as they are already bound"))
-        return;
-
     // Bind to first level of tree
     if (ArrayParams.empty())
+    {
+        // Check if identifiers are already bound
+        if (NsbAssert(Arrays[pLine->Params[0]].Members[0].first.empty(),
+            "Cannot bind identifiers to tree as they are already bound"))
+            return;
+
         for (uint32_t i = 1; i < Params.size(); ++i)
             Arrays[pLine->Params[0]].Members[i - 1].first = Params[i].Value;
+    }
     // Bind to subtree
     else
         for (uint32_t i = 1; i < Params.size(); ++i)