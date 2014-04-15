caa774d4418e98fd7f962654f1a62d13723cabf2
Mislav Blažević <krofnica996@gmail.com>
2014-04-15 20:45:02 +0200

    Implement Exit

--- src/nsbinterpreter.cpp
+++ src/nsbinterpreter.cpp
@@ -121,6 +121,7 @@ BreakOnAssert(false)
     Builtins[MAGIC_SOUND_AMPLITUDE] = &NsbInterpreter::SoundAmplitude;
     Builtins[MAGIC_SELECT] = &NsbInterpreter::Select;
     Builtins[MAGIC_CASE_BEGIN] = &NsbInterpreter::CaseBegin;
+    Builtins[MAGIC_EXIT] = &NsbInterpreter::Exit;
     Builtins[MAGIC_UNK90] = &NsbInterpreter::UNK90;
     //Builtins[MAGIC_CREATE_PROCESS] = &NsbInterpreter::CreateProcess;
     //Builtins[MAGIC_SET_FONT_ATTRIBUTES] = &NsbInterpreter::SetFontAttributes;
