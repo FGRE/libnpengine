dbc998efb5f7b9f6937a69ef90542f18c3614fdd
Mislav Blažević <krofnica996@gmail.com>
2014-08-04 01:56:10 +0200

    Move NSBContext and Variable to separate files

---
+++ include/NSBContext.hpp
@@ -0,0 +1,69 @@
+/* 
+ * libnpengine: Nitroplus script interpreter
+ * Copyright (C) 2014 Mislav Blažević <krofnica996@gmail.com>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * */
+#ifndef NSB_CONTEXT_HPP
+#define NSB_CONTEXT_HPP
+
+#include <stack>
+#include <cstdint>
+#include <string>
+using namespace std;
+
+class ScriptFile;
+class Line;
+class NSBContext
+{
+    struct StackFrame
+    {
+        ScriptFile* pScript;
+        uint32_t SourceLine;
+    };
+public:
+    NSBContext(const string& Name);
+    ~NSBContext();
+
+    bool Call(ScriptFile* pScript, const string& Symbol);
+    void Jump(const string& Symbol);
+    void Break();
+    const string& GetParam(uint32_t Index);
+    int GetNumParams();
+    const string& GetScriptName();
+    ScriptFile* GetScript();
+    Line* GetLine();
+    uint32_t GetMagic();
+    uint32_t Advance();
+    void Return();
+    void PushBreak();
+    void PopBreak();
+    void Wait(int32_t Time, bool Interrupt);
+    void Wake();
+    void TryWake();
+    bool IsStarving();
+    bool IsSleeping();
+
+private:
+    StackFrame* GetFrame();
+
+    const string Name;
+    uint64_t WaitTime;
+    uint64_t WaitStart;
+    bool WaitInterrupt;
+    stack<StackFrame> CallStack;
+    stack<string> BreakStack;
+};
+
+#endif
