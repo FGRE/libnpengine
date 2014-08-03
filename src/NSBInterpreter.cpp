fb48337996598b58e8e07697608aad82c13b4f75
Mislav Blažević <krofnica996@gmail.com>
2014-08-04 01:56:10 +0200

    Move NSBContext and Variable to separate files

--- src/NSBInterpreter.cpp
+++ src/NSBInterpreter.cpp
@@ -16,6 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * */
 #include "NSBInterpreter.hpp"
+#include "NSBContext.hpp"
 #include "Texture.hpp"
 #include "Window.hpp"
 #include "nsbmagic.hpp"
@@ -26,102 +27,8 @@
 #include <iostream>
 #include <memory>
 #include <algorithm>
-#include <chrono>
-using namespace std::chrono;
 
 #define NSB_ERROR(MSG1, MSG2) cout << __PRETTY_FUNCTION__ << ": " << MSG1 << " " << MSG2 << endl;
-
-Variable::Variable() : Literal(true)
-{
-}
-
-Variable::~Variable()
-{
-    Destroy();
-}
-
-void Variable::Initialize(int32_t Int)
-{
-    Val.Int = Int;
-    Tag = INT;
-}
-
-void Variable::Initialize(const string& Str)
-{
-    Val.Str = new string(Str);
-    Tag = STRING;
-}
-
-void Variable::Initialize(Variable* pVar)
-{
-    if (pVar->IsString())
-        Initialize(pVar->ToString());
-    else
-        Initialize(pVar->ToInt());
-}
-
-void Variable::Destroy()
-{
-    if (Tag == STRING)
-        delete Val.Str;
-}
-
-Variable* Variable::MakeInt(int32_t Int)
-{
-    Variable* pVar = new Variable;
-    pVar->Initialize(Int);
-    return pVar;
-}
-
-Variable* Variable::MakeString(const string& Str)
-{
-    Variable* pVar = new Variable;
-    pVar->Initialize(Str);
-    return pVar;
-}
-
-Variable* Variable::MakeCopy(Variable* pVar)
-{
-    Variable* pNew = new Variable;
-    pNew->Initialize(pVar);
-    return pNew;
-}
-
-int32_t Variable::ToInt()
-{
-    if (IsString())
-    {
-        NSB_ERROR("Converting String to Int", ToString());
-        return 0;
-    }
-    return Val.Int;
-}
-
-string Variable::ToString()
-{
-    if (IsInt())
-    {
-        NSB_ERROR("Converting Int to String", ToInt());
-        return "";
-    }
-    return *Val.Str;
-}
-
-bool Variable::IsInt()
-{
-    return Tag == INT;
-}
-
-bool Variable::IsString()
-{
-    return Tag == STRING;
-}
-
-void Variable::Set(Variable* pVar)
-{
-    Destroy();
-    Initialize(pVar);
-}
 
 ArrayVariable* ArrayVariable::MakeCopy(Variable* pVar)
 {
@@ -155,121 +62,6 @@ ArrayVariable* ArrayVariable::Find(int32_t Index)
 void ArrayVariable::Push(ArrayVariable* pVar)
 {
     Members.push_back(make_pair(string(), pVar));
-}
-
-NSBContext::NSBContext(const string& Name) : Name(Name), WaitTime(0), WaitStart(0), WaitInterrupt(false)
-{
-}
-
-NSBContext::~NSBContext()
-{
-}
-
-bool NSBContext::Call(ScriptFile* pScript, const string& Symbol)
-{
-    uint32_t CodeLine = pScript->GetSymbol(Symbol);
-    if (CodeLine == NSB_INVALIDE_LINE)
-        return false;
-
-    CallStack.push({pScript, CodeLine - 1});
-    return true;
-}
-
-void NSBContext::Jump(const string& Symbol)
-{
-    uint32_t CodeLine = GetScript()->GetSymbol(Symbol);
-    if (CodeLine != NSB_INVALIDE_LINE)
-        GetFrame()->SourceLine = CodeLine - 1;
-}
-
-void NSBContext::Break()
-{
-    Jump(BreakStack.top());
-}
-
-const string& NSBContext::GetScriptName()
-{
-    return GetScript()->GetName();
-}
-
-ScriptFile* NSBContext::GetScript()
-{
-    return GetFrame()->pScript;
-}
-
-Line* NSBContext::GetLine()
-{
-    return GetScript()->GetLine(GetFrame()->SourceLine);
-}
-
-const string& NSBContext::GetParam(uint32_t Index)
-{
-    return GetLine()->Params[Index];
-}
-
-int NSBContext::GetNumParams()
-{
-    return GetLine()->Params.size();
-}
-
-uint32_t NSBContext::GetMagic()
-{
-    return GetLine()->Magic;
-}
-
-NSBContext::StackFrame* NSBContext::GetFrame()
-{
-    return &CallStack.top();
-}
-
-uint32_t NSBContext::Advance()
-{
-    GetFrame()->SourceLine++;
-    return GetMagic();
-}
-
-void NSBContext::Return()
-{
-    CallStack.pop();
-}
-
-void NSBContext::PushBreak()
-{
-    BreakStack.push(GetParam(0));
-}
-
-void NSBContext::PopBreak()
-{
-    BreakStack.pop();
-}
-
-void NSBContext::Wait(int32_t Time, bool Interrupt)
-{
-    WaitInterrupt = Interrupt;
-    WaitTime = Time;
-    WaitStart = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
-}
-
-void NSBContext::Wake()
-{
-    WaitTime = 0;
-}
-
-void NSBContext::TryWake()
-{
-    if (WaitInterrupt)
-        Wake();
-}
-
-bool NSBContext::IsStarving()
-{
-    return CallStack.empty();
-}
-
-bool NSBContext::IsSleeping()
-{
-    uint64_t Now = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
-    return (Now - WaitStart) < WaitTime;
 }
 
 NSBInterpreter::NSBInterpreter(Window* pWindow) :
