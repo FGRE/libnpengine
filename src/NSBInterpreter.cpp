9ca3026f361d9f3a55a7be3d57875fe1a1f60cac
Mislav Blažević <krofnica996@gmail.com>
2014-12-25 19:32:09 +0100

    Better stub for SetFont and fix string segment ordering in text

--- src/NSBInterpreter.cpp
+++ src/NSBInterpreter.cpp
@@ -1379,12 +1379,12 @@ void NSBInterpreter::ClearBacklog()
 
 void NSBInterpreter::SetFont()
 {
-    /*string Font = */PopString();
-    /*int32_t Size = */PopInt();
-    /*uint32_t InColor = */PopColor();
-    /*uint32_t OutColor = */PopColor();
-    /*int32_t Weight = */PopInt();
-    /*string unk = */PopString();
+    Text::Font = PopString();
+    Text::Size = PopInt();
+    Text::InColor = PopColor();
+    Text::OutColor = PopColor();
+    Text::Weight = PopInt();
+    Text::Alignment = PopString();
 }
 
 void NSBInterpreter::SetShortcut()
